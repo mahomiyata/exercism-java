2262449d97f79610d07be53093231963669d3d79
Maho Miyata <mahomiyata@users.noreply.github.com>
2022-02-16 16:33:10 +0900

    completed

--- cars-assemble/src/main/java/CarsAssemble.java
+++ cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,23 @@
 public class CarsAssemble {
 
+    int baseProducedCarsPerHour = 221;
+
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.productionRateperHour() method");
+        double productionRate = 0.0;
+        if (speed >= 1 && speed <= 4) {
+            productionRate = baseProducedCarsPerHour * speed;
+        } else if (speed >= 5 && speed <= 8) {
+            productionRate = baseProducedCarsPerHour * speed * 0.9;
+        } else if (speed == 9) {
+            productionRate = baseProducedCarsPerHour * speed * 0.8;
+        } else if (speed == 10) {
+            productionRate = baseProducedCarsPerHour * speed * 0.77;
+        }
+        return productionRate;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.workingItemsPerMinute() method");
+        int productionRate = (int)(productionRatePerHour(speed) / 60);
+        return productionRate;
     }
 }
