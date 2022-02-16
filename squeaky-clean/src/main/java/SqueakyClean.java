a7c621a9360b1445e8d7580007f95afacdc01930
Maho Miyata <mahomiyata@users.noreply.github.com>
2022-02-16 15:34:07 +0900

    completed

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,30 @@
 class SqueakyClean {
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+
+        StringBuilder sb = new StringBuilder();
+        boolean isAfterDash = false;
+        char[] charArray = identifier.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+            if (isAfterDash) {
+                sb.append(Character.toUpperCase(charArray[i]));
+                isAfterDash = false;
+            } else if(Character.isSpaceChar(charArray[i])) {
+                sb.append('_');
+            } else if (Character.isISOControl(charArray[i])) {
+                sb.append("CTRL");
+            } else if (charArray[i] == '-') {
+                isAfterDash = true;
+            } else if(isGreekLEtter(charArray[i])) {
+                continue;
+            } else if(Character.isLetter(charArray[i])){
+                sb.append(charArray[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    static boolean isGreekLEtter(char c) {
+        return (c >= 'α' && c <= 'ω');
     }
 }
