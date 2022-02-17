c7f89fe326b7cce741f496fe7ede02f2f19e469a
Maho Miyata <mahomiyata@users.noreply.github.com>
2022-02-18 01:39:58 +0900

    completed

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,9 +1,28 @@
 public class Hamming {
+
+    String leftStrand;
+    String rightStrand;
+
     public Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (leftStrand.length() == rightStrand.length()) {
+            this.leftStrand = leftStrand;
+            this.rightStrand = rightStrand;
+        } else if (leftStrand.length() == 0) {
+            throw new IllegalArgumentException("left strand must not be empty.");
+        } else if (rightStrand.length() == 0) {
+            throw new IllegalArgumentException("right strand must not be empty.");
+        } else {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
     public int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int counter = 0;
+        for (int i = 0; i < this.leftStrand.length(); i++) {
+            if (this.leftStrand.charAt(i) != this.rightStrand.charAt(i)) {
+                counter += 1;
+            }
+        }
+        return counter;
     }
 }
