8608fd3c0487d9bd533c2249dab3cf969d1ed9e0
Maho Miyata <mahomiyata@users.noreply.github.com>
2022-02-17 22:27:50 +0900

    completed

--- need-for-speed/src/main/java/NeedForSpeed.java
+++ need-for-speed/src/main/java/NeedForSpeed.java
@@ -1,27 +1,56 @@
 class NeedForSpeed {
     // TODO: define the constructor for the 'NeedForSpeed' class
 
+    int speed;
+    int batteryDrain;
+    int distance;
+    int batteryPercentage;
+
+    public NeedForSpeed(int speed, int batteryDrain) {
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
+        this.distance = 0;
+        this.batteryPercentage = 100;
+    }
+
     public boolean batteryDrained() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.batteryDrained() method");
+        return batteryPercentage < batteryDrain;
     }
 
     public int distanceDriven() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.distanceDriven() method");
+        return this.distance;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    public int getBatteryDrain() {
+        return this.batteryDrain;
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.drive() method");
+        if (batteryPercentage >= batteryDrain) {
+            this.distance += this.speed;
+            this.batteryPercentage -= this.batteryDrain;
+        }
     }
 
     public static NeedForSpeed nitro() {
-        throw new UnsupportedOperationException("Please implement the (static) NeedForSpeed.nitro() method");
+        return new NeedForSpeed(50, 4);
     }
 }
 
 class RaceTrack {
     // TODO: define the constructor for the 'RaceTrack' class
 
+    int distance;
+    public RaceTrack(int distance) {
+        this.distance = distance;
+    }
+
     public boolean carCanFinish(NeedForSpeed car) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack.carCanFinish() method");
+        int driveCount = 100 / car.getBatteryDrain();
+        return driveCount * car.getSpeed() >= this.distance;
     }
 }
