df0a341dd58ce3afed27dce8628bd0f403b50c3b
Maho Miyata <mahomiyata@users.noreply.github.com>
2022-02-18 01:21:27 +0900

    completed

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,20 @@
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder sb = new StringBuilder();
+        if (number % 3 == 0) {
+            sb.append("Pling");
+        }
+        if (number % 5 == 0) {
+            sb.append("Plang");
+        }
+        if (number % 7 == 0) {
+            sb.append("Plong");
+        }
+        if (number % 3 != 0 && number % 5 != 0 && number % 7 != 0) {
+            sb.append(Integer.toString(number));
+        }
+        return sb.toString();
     }
 
 }
