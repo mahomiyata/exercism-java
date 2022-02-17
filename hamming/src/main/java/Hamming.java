e4fd315dcc38dcd69b2516c3676ea7bb966d1c35
Maho Miyata <mahomiyata@users.noreply.github.com>
2022-02-18 01:44:40 +0900

    refactor

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -2,14 +2,23 @@ public class Hamming {
 
     String leftStrand;
     String rightStrand;
+    int hammingDistance;
 
     public Hamming(String leftStrand, String rightStrand) {
         if (leftStrand.length() == rightStrand.length()) {
             this.leftStrand = leftStrand;
             this.rightStrand = rightStrand;
-        } else if (leftStrand.length() == 0) {
+            this.hammingDistance = 0;
+
+            for (int i = 0; i < this.leftStrand.length(); i++) {
+                if (this.leftStrand.charAt(i) != this.rightStrand.charAt(i)) {
+                    this.hammingDistance += 1;
+                }
+            }
+
+        } else if (leftStrand.isEmpty()) {
             throw new IllegalArgumentException("left strand must not be empty.");
-        } else if (rightStrand.length() == 0) {
+        } else if (rightStrand.isEmpty()) {
             throw new IllegalArgumentException("right strand must not be empty.");
         } else {
             throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
@@ -17,12 +26,6 @@ public class Hamming {
     }
 
     public int getHammingDistance() {
-        int counter = 0;
-        for (int i = 0; i < this.leftStrand.length(); i++) {
-            if (this.leftStrand.charAt(i) != this.rightStrand.charAt(i)) {
-                counter += 1;
-            }
-        }
-        return counter;
+        return this.hammingDistance;
     }
 }
