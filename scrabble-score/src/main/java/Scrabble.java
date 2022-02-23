a8b26c5c93080665e3d821a35f9607c2e2de6e6a
Maho Miyata <mahomiyata@users.noreply.github.com>
2022-02-23 22:25:48 +0900

    completed

---
+++ scrabble-score/src/main/java/Scrabble.java
@@ -0,0 +1,59 @@
+class Scrabble {
+
+    private int score;
+
+    Scrabble(String word) {
+        char[] letters = word.toCharArray();
+        for(char letter: letters) {
+            switch (Character.toUpperCase(letter)) {
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                case 'L':
+                case 'N':
+                case 'R':
+                case 'S':
+                case 'T':
+                    score += 1;
+                    break;
+                case 'D':
+                case 'G':
+                    score += 2;
+                    break;
+                case 'B':
+                case 'C':
+                case 'M':
+                case 'P':
+                    score += 3;
+                    break;
+                case 'F':
+                case 'H':
+                case 'V':
+                case 'W':
+                case 'Y':
+                    score += 4;
+                    break;
+                case 'K':
+                    score += 5;
+                    break;
+                case 'J':
+                case 'X':
+                    score += 8;
+                    break;
+                case 'Q':
+                case 'Z':
+                    score += 10;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    int getScore() {
+        return score;
+    }
+
+}
